13857a5fde9f812ac34cc7e33b6ae1461bb36091
Lee-Jon <leejonball@gmail.com>
2014-11-28 10:14:19 +0000

    Include tests (Rspec) for existing functionality
    We have included 23 tests, all passing, for existing functionality of
    Gilded Rose's update_quality method. The original test has been removed.
    A spec_helper patches the GildedRose.initialize method, so we can inject
    a custom Item object for test into the @items parameter.
    
    Normal, Brie, Legendary, Backstage Passes are all tested for a drop in
    sell in date, not exceeding 50 quality on increases, not going past 0
    quality in decreases, and boundaries of being on the sell_in and past
    the sell_in date.
    
    Aged Brie increases in quality twice as fast when its past its sell in
    date. This is not clear in the original specification so the test case
    matches the code.
    
    Changes to be committed:
      modified:   gilded_rose_spec.rb
      new file:   gilded_rose_spec_helper.rb

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -1,10 +1,207 @@
 require './gilded_rose.rb'
+require './gilded_rose_spec_helper.rb'
+
 require "rspec"
 
 describe GildedRose do
 
-  it "should do something" do
-    subject.update_quality
+  describe "Updating Normal Items" do
+    describe "while in date" do
+      before do
+        @item = Item.new("Normal Item", 10, 1)
+        @gildedrose = GildedRose.new(@item)
+        @gildedrose.update_quality
+      end
+
+      it "should decrease the sell in" do
+        expect(@item.sell_in).to eq(9)
+      end
+
+      it "should decrease the quality" do
+        expect(@item.quality).to eq(0)
+      end
+
+      it "should not decrease the quality below zero" do
+        @gildedrose.update_quality
+
+        expect(@item.quality).to_not be < 0
+      end
+    end
+
+    describe "when sell in hits zero" do
+      before do
+        @item = Item.new("Normal Item", 1, 10)
+        @gildedrose = GildedRose.new(@item)
+        @gildedrose.update_quality
+      end
+
+      it "should have a zero sell in" do
+        expect(@item.sell_in).to eq(0)
+      end
+
+      it "should have decreased quality by 1" do
+        expect(@item.quality).to eq(9)
+      end
+    end
+
+    describe "when sell in passes zero" do
+      before do
+        @item = Item.new("Normal Item", 0, 10)
+        @gildedrose = GildedRose.new(@item)
+        @gildedrose.update_quality
+      end
+
+      it "should have a negative sell in" do
+        expect(@item.sell_in).to eq(-1)
+      end
+
+      it "should have decreased quality by 2" do
+        expect(@item.quality).to eq(8)
+      end
+    end
   end
 
+  describe "for legendary items" do
+    before do
+      @item = Item.new("Sulfuras, Hand of Ragnaros", 0, 80)
+      @gildedrose = GildedRose.new(@item)
+
+      @gildedrose.update_quality
+    end
+
+    it "should not change sell in" do
+      expect(@item.sell_in).to eq(0)
+    end
+
+    it "should not change quality" do
+      expect(@item.quality).to eq(80)
+    end
+  end
+
+  describe "for Aged Brie" do
+    describe "before sell in" do
+      before do
+        @item = Item.new("Aged Brie", 2, 0)
+        @gildedrose = GildedRose.new(@item)
+
+        @gildedrose.update_quality
+      end
+
+      it "should decrease the sell in" do
+        expect(@item.sell_in).to eq(1)
+      end
+
+      it "should increase in quality" do
+        expect(@item.quality).to eq(1)
+      end
+
+      it "should not increase beyond 50" do
+        51.times { @gildedrose.update_quality }
+
+        expect(@item.quality).to_not be > 50
+      end
+    end
+
+    describe "approaching sell in" do
+      before do
+        @item = Item.new("Aged Brie", 1, 0)
+        @gildedrose = GildedRose.new(@item)
+
+        @gildedrose.update_quality
+      end
+
+      it "should have zero sell in date" do
+        expect(@item.sell_in).to eq(0)
+      end
+
+      it "increases in age when older" do
+        expect(@item.quality).to eq(1)
+      end
+    end
+
+    describe "after sell in" do
+      before do
+        @item = Item.new("Aged Brie", -5, 0)
+        @gildedrose = GildedRose.new(@item)
+
+        @gildedrose.update_quality
+      end
+
+      it "should decrease the sell in" do
+        expect(@item.sell_in).to eq(-6)
+      end
+
+      it "increases in age when older" do
+        expect(@item.quality).to eq(2)
+      end
+    end
+
+    describe "with maximum quality" do
+      before do
+        @item = Item.new("Aged Brie", 10, 50)
+        @gildedrose = GildedRose.new(@item)
+
+        @gildedrose.update_quality
+      end
+
+      it "should not increase quality beyond 50" do
+        expect(@item.quality).to_not be > 50
+      end
+    end
+  end
+
+  describe "for Backstage Passes" do
+    before do
+      @item = Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)
+      @gildedrose = GildedRose.new(@item)
+    end
+
+    it "should decrease sell in" do
+      @gildedrose.update_quality
+
+      expect(@item.sell_in).to eq (14)
+    end
+
+    describe "increases in quality" do
+      it "should be by 1 when sell_in is 15-10" do
+        5.times { @gildedrose.update_quality }
+
+        expect(@item.quality).to eq(20 + 5)
+      end
+
+      it "should be by 2 when sell_in < 10" do
+        5.times { @gildedrose.update_quality }
+        1.times { @gildedrose.update_quality }
+
+        expect(@item.quality).to eq(20 + (5*1) + (1*2))
+      end
+
+      it "should be by 3 when sell_in < 5" do
+        5.times { @gildedrose.update_quality } # 10 days
+        5.times { @gildedrose.update_quality } # 5 days
+        1.times { @gildedrose.update_quality } # 4 days + 3
+
+        expect(@item.quality).to eq(20 + (5*1) + (5*2) + (1*3))
+      end
+    end
+
+    describe "after sell it" do
+      it "should go to 0" do
+        @item.sell_in.times { @gildedrose.update_quality }
+        @gildedrose.update_quality
+
+        expect(@item.quality).to eq(0)
+      end
+    end
+
+    describe "with maximum quality" do
+      it "should not get higher than 50 quality" do
+        @item = Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 49)
+        @gildedrose = GildedRose.new(@item)
+        11.times { @gildedrose.update_quality }
+
+        expect(@item.quality).to_not be > 50
+      end
+    end
+  end
 end
---
+++ gilded_rose_spec_helper.rb
@@ -0,0 +1,21 @@
+# gilded_rose_spec_helper.rb
+
+class GildedRose
+  attr_reader :items
+
+  def initialize(object=true)
+    @items = []
+
+    if object == true
+      @items << Item.new("+5 Dexterity Vest", 10, 20)
+      @items << Item.new("Aged Brie", 2, 0)
+      @items << Item.new("Elixir of the Mongoose", 5, 7)
+      @items << Item.new("Sulfuras, Hand of Ragnaros", 0, 80)
+      @items << Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)
+      @items << Item.new("Conjured Mana Cake", 3, 6)
+    else
+      @items << object
+    end
+  end
+
+end
