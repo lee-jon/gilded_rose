4a0252de9287f607ff229682a988d7826d705b36
Joe James <joe@joejames.io>
2014-12-19 16:00:53 +0000

    remove magic numbers within assertions

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -6,25 +6,32 @@ require "rspec"
 
 describe GildedRose do
   let(:gilded_rose) { GildedRose.new(item) }
+  let(:zero_quality) { 0 }
+  let(:max_quality) { 50 }
+  let(:zero_sell_in) { 0 }
 
   describe "Updating Normal Items" do
     describe "while in date" do
-      let(:item) { Item.new("Normal Item", 10, 1) }
+      let(:sell_in) { 10 }
+      let(:quality) { 1 }
+      let(:item) { Item.new("Normal Item", sell_in, quality) }
 
       it "should decrease the sell in" do
         expect{ gilded_rose.update_quality }.
           to change{ item.sell_in }.
-          from(10).to(9)
+          to(sell_in - 1)
       end
 
       it "should decrease the quality" do
         expect{ gilded_rose.update_quality }.
           to change{ item.quality }.
-          from(1).to(0)
+          to(zero_quality)
       end
 
       context "with zero quality items" do
-        let(:item) { Item.new("Normal Item", 10, 0) }
+        let(:sell_in) { 10 }
+        let(:quality) { 0 }
+        let(:item) { Item.new("Normal Item", sell_in, quality) }
 
         it "should not decrease the quality below zero" do
           expect{ gilded_rose.update_quality}.
@@ -34,40 +41,46 @@ describe GildedRose do
     end
 
     describe "when sell in hits zero" do
-      let(:item) { Item.new("Normal Item", 1, 10) }
+      let(:sell_in) { 1 }
+      let(:quality) { 10 }
+      let(:item) { Item.new("Normal Item", sell_in, quality) }
 
       it "should have a zero sell in" do
         expect{ gilded_rose.update_quality }.
           to change{ item.sell_in }.
-          from(1).to(0)
+          to(zero_sell_in)
       end
 
       it "should have decreased quality by 1" do
         expect{ gilded_rose.update_quality }.
           to change{ item.quality }.
-          from(10).to(9)
+          to(quality - 1)
       end
     end
 
     describe "when sell in passes zero" do
-      let(:item) { Item.new("Normal Item", 0, 10) }
+      let(:sell_in) { 0 }
+      let(:quality) { 10 }
+      let(:item) { Item.new("Normal Item", sell_in, quality) }
 
       it "should have a negative sell in" do
         expect{ gilded_rose.update_quality }.
           to change{ item.sell_in }.
-          from(0).to(-1)
+          to(zero_sell_in - 1)
       end
 
       it "should have decreased quality by 2" do
         expect{ gilded_rose.update_quality }.
           to change{ item.quality }.
-          from(10).to(8)
+          to(quality - 2)
       end
     end
   end
 
   describe "for legendary items" do
-    let(:item) { Item.new("Sulfuras, Hand of Ragnaros", 0, 80) }
+    let(:sell_in) { 0 }
+    let(:quality) { 80 }
+    let(:item) { Item.new("Sulfuras, Hand of Ragnaros", sell_in, quality) }
 
     it "should not change sell in" do
       expect{ gilded_rose.update_quality }.
@@ -82,62 +95,70 @@ describe GildedRose do
 
   describe "for Aged Brie" do
     describe "before sell in" do
-      let(:item) { Item.new("Aged Brie", 2, 0) }
+      let(:sell_in) { 2 }
+      let(:quality) { zero_quality }
+      let(:item) { Item.new("Aged Brie", sell_in, quality) }
 
       it "should decrease the sell in" do
         expect{ gilded_rose.update_quality }.
           to change{ item.sell_in }.
-          from(2).to(1)
+          to(sell_in - 1)
       end
 
       it "should increase in quality" do
         expect{ gilded_rose.update_quality }.
           to change{ item.quality }.
-          from(0).to(1)
+          to(quality + 1)
       end
 
       it "should not increase beyond 50" do
         expect{
           52.times { gilded_rose.update_quality }
         }.to change{ item.quality }.
-          from(0).to(50)
+          to(max_quality)
       end
     end
 
     describe "approaching sell in" do
-      let(:item) { Item.new("Aged Brie", 1, 0) }
+      let(:sell_in) { zero_sell_in + 1 }
+      let(:quality) { zero_quality }
+      let(:item) { Item.new("Aged Brie", sell_in, quality) }
 
       it "should have zero sell in date" do
         expect{ gilded_rose.update_quality }.
           to change{ item.sell_in }.
-          from(1).to(0)
+          to(zero_sell_in)
       end
 
       it "increases in age when older" do
         expect{ gilded_rose.update_quality }.
           to change{ item.quality }.
-          from(0).to(1)
+          to(quality + 1) # does this represent age??
       end
     end
 
     describe "after sell in" do
-      let(:item) { Item.new("Aged Brie", -5, 0) }
+      let(:sell_in) { -5 }
+      let(:quality) { 0 }
+      let(:item) { Item.new("Aged Brie", sell_in, quality) }
 
       it "should decrease the sell in" do
         expect{ gilded_rose.update_quality }.
           to change{ item.sell_in }.
-          from(-5).to(-6)
+          to(sell_in - 1)
       end
 
       it "increases in age when older" do
         expect{ gilded_rose.update_quality }.
           to change{ item.quality }.
-          from(0).to(2)
+          to(quality + 2)
       end
     end
 
     describe "with maximum quality" do
-      let(:item) { Item.new("Aged Brie", 10, 50) }
+      let(:sell_in) { 10 }
+      let(:quality) { max_quality }
+      let(:item) { Item.new("Aged Brie", sell_in, quality) }
 
       it "should not increase quality beyond 50" do
         expect{ gilded_rose.update_quality }.
@@ -147,12 +168,14 @@ describe GildedRose do
   end
 
   describe "for Backstage Passes" do
-    let(:item) { Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20) }
+    let(:sell_in) { 15 }
+    let(:quality) { 20 }
+    let(:item) { Item.new("Backstage passes to a TAFKAL80ETC concert", sell_in, quality) }
 
     it "should decrease sell in" do
       expect{ gilded_rose.update_quality }.
         to change{ item.sell_in }.
-        from(15).to(14)
+        to(sell_in - 1)
     end
 
     describe "increases in quality" do
@@ -160,7 +183,7 @@ describe GildedRose do
         expect{
           5.times { gilded_rose.update_quality }
         }.to change{ item.quality}.
-          from(20).to(25)
+          to(quality + 5)
       end
 
       it "should be by 2 when sell_in < 10" do
@@ -168,7 +191,7 @@ describe GildedRose do
           5.times { gilded_rose.update_quality }
           1.times { gilded_rose.update_quality }
         }.to change{ item.quality }.
-          from(20).to(20 + (5*1) + (1*2))
+          from(quality).to(quality + (5*1) + (1*2))
       end
 
       it "should be by 3 when sell_in < 5" do
@@ -177,7 +200,7 @@ describe GildedRose do
           5.times { gilded_rose.update_quality } # 5 days
           1.times { gilded_rose.update_quality } # 4 days + 3
         }.to change{ item.quality }.
-          from(20).to(20 + (5*1) + (5*2) + (1*3))
+          to(quality + (5*1) + (5*2) + (1*3))
       end
     end
 
@@ -187,17 +210,20 @@ describe GildedRose do
           item.sell_in.times { gilded_rose.update_quality }
           gilded_rose.update_quality
         }.to change{ item.quality }.
-          from(20).to(0)
+          to(zero_quality)
       end
     end
 
     describe "with maximum quality" do
-      let(:item) { Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 49) }
+      let(:sell_in) { 15 }
+      let(:quality) { 49 }
+      let(:item) { Item.new("Backstage passes to a TAFKAL80ETC concert", sell_in, quality) }
+
       it "should not get higher than 50 quality" do
         expect{
           11.times { gilded_rose.update_quality }
         }.to change{ item.quality }.
-          from(49).to(50)
+          to(max_quality)
       end
     end
   end
